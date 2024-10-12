f325191b29b607b816473c541d923ddedd415d39
BrayDog2010 <113742627+BrayDog2010@users.noreply.github.com>
2024-10-12 04:07:28 +0000

    1.1.3 Fabric Update

---
+++ src/client/java/com/BrayDog2010/ChippedCreateFabricClient.java
@@ -0,0 +1,10 @@
+package com.BrayDog2010;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChippedCreateFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
